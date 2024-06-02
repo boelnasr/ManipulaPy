8f32003668fe0d835bcaeca2b83f7215f2dfd019
Mohamed Aboelnasr <aboelnasr1997@gmail.com>
2024-06-02 13:34:58 +0200

    Auto stash before rebase of "main" onto "origin/main"

--- setup.py
+++ setup.py
@@ -32,38 +32,6 @@ setup(
             "ManipulaPy_data/ur5/visual/*.dae",
             "ManipulaPy_data/xarm/xarm6_robot.urdf",
             "ManipulaPy_data/xarm/visual/*.dae",
-=======
-    name='ManipulaPy',
-    version='1.0.1',
-    author='Mohamed Aboelnar',
-    author_email='aboelnasr1997@gmail.com',
-    description='A package for robotic serial manipulator operations',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/boelnasr/ManipulaPy',
-    packages=find_packages(),
-    install_requires=[
-        'numpy>=1.19.2',
-        'scipy>=1.5.2',
-        'urchin>=0.0.27',
-        'pybullet>=3.0.6',
-        'pycuda>=2021.1',
-        'trimesh>=3.9.14'  # Add trimesh if not already included
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-    include_package_data=True,
-    package_data={
-        'ManipulaPy': [
-            'ManipulaPy_data/ur5/ur5.urdf',
-            'ManipulaPy_data/ur5/visual/*.dae',
-            'ManipulaPy_data/xarm/xarm6_robot.urdf',
-            'ManipulaPy_data/xarm/visual/*.dae'
->>>>>>> b175ebfc3e1929301748e949aaf8e848927935c5
         ],
     },
 )
